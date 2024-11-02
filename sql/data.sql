962719e1193de88e6c5dc65e19fe47f0f1cc5d26
JohnCarloTulin <167319067+JohnCarloTulin@users.noreply.github.com>
2024-11-02 11:18:04 +0800

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,28 +1,28 @@
-CREATE TABLE `bicycle_manager_login` (
-  `staff_manager_id` int(100) AUTO_INCREMENT PRIMARY KEY,
-  `username` varchar(1024),
-  `password` varchar(1024),
-  `first_name` varchar(50),
-  `last_name` varchar(50)
+CREATE TABLE bicycle_manager_login (
+  staff_manager_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(1024),
+  password VARCHAR(1024),
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
 );
 
-CREATE TABLE `bike_store_employees` (
-  `employee_id` int(30) AUTO_INCREMENT PRIMARY KEY,
-  `assigned_job` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `date_of_birth` varchar(50),
-  `employee_email` text,
-  `date_added` timestamp DEFAULT current_timestamp()
+CREATE TABLE bike_store_employees (
+  employee_id INT AUTO_INCREMENT PRIMARY KEY,
+  assigned_job VARCHAR(50),
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  date_of_birth VARCHAR(50),
+  employee_email TEXT,
+  date_added TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
 
 CREATE TABLE `progress_of_employees` (
-  `log_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `accomplishment_of_employee` varchar(50),
-  `description_of_accomplishment` text,
-  `employee_id` int(11),
-  `date_added` timestamp DEFAULT current_timestamp(),
-  `recorded_by` varchar(50),
-  `last_updated_by` varchar(50),
-  `last_updated` timestamp
+  log_id INT AUTO_INCREMENT PRIMARY KEY,
+  accomplishment_of_employee VARCHAR(50),
+  description_of_accomplishment TEXT,
+  employee_id INT(11),
+  date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  recorded_by VARCHAR(50),
+  last_updated_by VARCHAR(50),
+  last_updated TIMESTAMP
 )
