20c126ddc13736ac08ac390b81a5807e3996065a
JohnCarloTulin <167319067+JohnCarloTulin@users.noreply.github.com>
2024-11-02 11:14:20 +0800

    Delete sql/bike_store_employees.sql

--- sql/bike_store_employees.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `bike_store_employees` (
-  `employee_id` int(30),
-  `assigned_job` varchar(50),
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `date_of_birth` varchar(50),
-  `employee_email` text,
-  `date_added` timestamp DEFAULT current_timestamp()
-)
